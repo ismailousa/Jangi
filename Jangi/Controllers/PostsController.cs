d9dde74c6979d518e0b5cc5a61197345386d9755
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-08-23 15:46:26 +0300

    Basic routing added

---
+++ Jangi/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jangi.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts
+        public ActionResult Index()
+        {
+            return Content("HomePage");
+        }
+    }
+}
