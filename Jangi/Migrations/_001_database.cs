84595b501510ad72c2a6559b407ed1add1c235a8
DESKTOP-LL4FE81\isma_ <isma_ilou@hotmail.com>
2017-08-25 18:01:26 +0300

    Database added using FluentMigrator

---
+++ Jangi/Migrations/_001_database.cs
@@ -0,0 +1,73 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Jangi.Migrations
+{
+    [Migration(1)]
+    public class _001_database : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Users");
+            Delete.Table("Posts");
+            Delete.Table("Tags");
+            Delete.Table("Post_Tags");
+            Delete.Table("Types");
+            Delete.Table("Comments");
+            Delete.Table("VoteType");
+            Delete.Table("Vote");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Users")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Pseudo").AsString(30)
+                .WithColumn("Email").AsString(256)
+                .WithColumn("BirthDate").AsDate()
+                .WithColumn("Password").AsString(128);
+
+            Create.Table("Posts")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Content").AsCustom("Text")
+                .WithColumn("Date").AsDateTime()
+                .WithColumn("Author").AsInt32().ForeignKey("Users", "id").OnDelete(Rule.Cascade);
+
+            Create.Table("Tags")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Tag").AsString(30)
+                .WithColumn("Author").AsInt32().ForeignKey("Users", "id").OnDelete(Rule.None);
+
+            Create.Table("Post_Tags")
+                .WithColumn("postID").AsInt32().ForeignKey("Posts", "id")
+                .WithColumn("tagID").AsInt32().ForeignKey("Tags", "id");
+
+            Create.Table("Types")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Name").AsString(30);
+
+            Create.Table("Comments")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Type").AsInt32().ForeignKey("Types", "id")
+                .WithColumn("Content").AsCustom("Text")
+                .WithColumn("Date").AsDateTime()
+                .WithColumn("Author").AsInt32().ForeignKey("Users", "id")
+                .WithColumn("Commented").AsInt32();
+
+            Create.Table("VoteType")
+               .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("Name").AsString(30);
+
+            Create.Table("Vote")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Type").AsInt32().ForeignKey("Types", "id")
+                .WithColumn("Voter").AsInt32().ForeignKey("Users", "id")
+                .WithColumn("VoteType").AsInt32().ForeignKey("VoteType", "id")
+                .WithColumn("Voted").AsInt32();
+        }
+    }
+}
